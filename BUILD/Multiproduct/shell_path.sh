ae2d44338af8211650892171e824bbe6f288fb8f
liyucheng <578284551@qq.com>
2020-11-17 14:04:06 +0800

    master.4

--- BUILD/Multiproduct/shell_path.sh
+++ BUILD/Multiproduct/shell_path.sh
@@ -1,5 +1,4 @@
-echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/liyucheng/project/project1/my-project/RELEASE/moda/libmoda.so" >> ~/.bashrc
-echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/liyucheng/project/project1/my-project/RELEASE/modb/libmodb.so" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/liyucheng/project/project1/my-project/RELEASE/moda:/home/liyucheng/project/project1/my-project/RELEASE/modb" >> ~/.zshrc
 
 
 
